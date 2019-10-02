a0957b4814ca5bda6b94bfc21b45b1411ab9e965
MiiaK <miiaknuuti@gmail.com>
2019-10-02 11:30:37 +0200

    Added main2 and completed main

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
-
-int main(){
+/*Den här approachen fungerar inte för att vi vill kunna lägga till en address med gatunummer. Till exempel Vikingavägen 13 */
+ int main(){
   char namn[20];
-  printf("Vad heter du?\n" );
-  scanf("%s\n",namn );
+  printf("Vad heter du?\n" ); fflush(stdout); /*fflush behövs för att annars buffras det istället! */
+  scanf("19%s",namn ); /*19%s för att namnet får vara max 19 långt */
   printf("Hej! %s\n", namn);
   return 0;
 }
