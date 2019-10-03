b5cb5b8d7898e82a13f88539425fcd7796c3de0f
MiiaK <miiaknuuti@gmail.com>
2019-10-03 10:57:35 +0200

    Solved exercise 2.8.5 a and b

--- ovning2.8.5/main.c
+++ ovning2.8.5/main.c
@@ -1,6 +1,22 @@
 #include<stdio.h>
-
+#define RABATT 0.1
+#define GRANS 1000
 int main(){
-  
-
+    float pris;
+    float antal;
+    float i;
+    printf("Hur mycket kostar varan?\n"); fflush(stdout);
+    scanf("%f", &pris);
+    printf("Hur många av varan köper du?\n"); fflush(stdout);
+    scanf("%f", &antal);
+    i = pris*antal;
+    if (i >GRANS)
+    {
+      printf("Kostnad: %.2f\n",i*(1-RABATT) );
+    }
+    else
+    {
+      printf("Kostnad: %.2f\n", i);
+    }
+    return 0;
 }
