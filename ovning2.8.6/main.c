1c1f5b516862d4ee373875d6eff96163c583911d
MiiaK <miiaknuuti@gmail.com>
2019-10-03 08:45:20 +0200

    Final solution

--- ovning2.8.6/main.c
+++ ovning2.8.6/main.c
@@ -7,9 +7,11 @@ int main()
   int input=0;
   int i;
   printf("För hur många nummer vill du generera tabellen?"); fflush(stdout); scanf("%d", &input);
-  printf("i   i*i  i*i*i \n"); fflush(stdout);
+  printf("i    i*i  i*i*i \n"); fflush(stdout);
+  printf("=    ===  ===== \n"); fflush(stdout);
+
   for (i=1;i<=input;i++)
   {
-  printf("%d %d %d \n", i, kvad(i), kub(i)); fflush(stdout);
+  printf("%d %6d %6d \n", i, kvad(i), kub(i)); fflush(stdout);
   }
 }
