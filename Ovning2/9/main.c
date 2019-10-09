f211b604760f9da84703feca8515957dcc409040
MiiaKay <miiaknuuti@gmail.com>
2019-10-09 17:28:47 +0200

    Solution to new exercise

---
+++ Ovning2/9/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<limits.h>
+
+int main(){
+  printf("CHAR_BIT: %lld \n", CHAR_BIT);
+  printf("SCHAR_MAX: %lld \n", SCHAR_MAX);
+  printf("SHRT_MAX: %lld \n", SHRT_MAX);
+  printf("USHRT_MAX: %lld \n", USHRT_MAX);
+  printf("INT_MAX: %lld \n", INT_MAX);
+  printf("UINT_MAX: %lld \n", UINT_MAX);
+  printf("LONG_MAX: %lld \n", LONG_MAX);
+  printf("ULONG_MAX: %llu \n", ULONG_MAX);
+}
